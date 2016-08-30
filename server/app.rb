b775e9c704baca8df9d55f007c265ca9218c977e
Brandan Lennox <brandan@bclennox.com>
2016-08-29 21:23:05 -0400

    Added a very basic Trip model.

--- server/app.rb
+++ server/app.rb
@@ -7,9 +7,9 @@ require_relative 'models/trip'
 require_relative 'models/person'
 
 get '/trips' do
-  Trip.all.to_json
+  { data: Trip.all }.to_json
 end
 
 get '/people' do
-  Person.all.to_json
+  { data: Person.all }.to_json
 end
--- server/models/application_model.rb
+++ server/models/application_model.rb
@@ -1,5 +1,17 @@
 module ApplicationModel
   def to_json(*args)
-    to_hash.to_json(*args)
+    {
+      type: self.class.name,
+      id: id,
+      attributes: serialized_attributes
+    }.to_json(*args)
+  end
+
+  private
+
+  def serialized_attributes
+    to_hash.tap do |attributes|
+      attributes.delete(:id)
+    end
   end
 end
