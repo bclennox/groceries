6636413224d8c9418ae4aaade88ddc84f99afe94
Brandan Lennox <brandan@bclennox.com>
2016-08-30 21:59:54 -0400

    Adding client-side relationship for Person and Trip.

--- client/app/models/person.js
+++ client/app/models/person.js
@@ -1,6 +1,6 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-  date: DS.attr('date'),
-  people: DS.hasMany('people')
+  name: DS.attr('string'),
+  trips: DS.hasMany('trips')
 });
