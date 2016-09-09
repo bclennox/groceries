1642d6c2d182bf5ac86ffe40a40165bb2251c58e
Brandan Lennox <brandan@bclennox.com>
2016-09-08 22:09:36 -0400

    Moving server calls to /api namespace.

--- server/app.rb
+++ server/app.rb
@@ -7,7 +7,7 @@ require_relative 'models/json_api_serializer'
 require_relative 'models/trip'
 require_relative 'models/person'
 
-get '/trips/:id' do
+get '/api/trips/:id' do
   trip = params[:id] == 'latest' ? Trip.latest : Trip[id: params[:id]]
   JsonApiSerializer.new(trip, relationships: :people).to_json
 end
