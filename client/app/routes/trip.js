91f50601c0e403934cfcf03e415488cddc0fc6d1
Brandan Lennox <brandan@bclennox.com>
2016-09-14 21:04:20 -0400

    Adding avatars and items.

--- client/app/routes/trip.js
+++ client/app/routes/trip.js
@@ -3,5 +3,10 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model(params) {
     return this.get('store').findRecord('trip', params.trip_id);
+  },
+
+  renderTemplate() {
+    this.render("masthead", { outlet: "masthead", into: "application" });
+    this.render();
   }
 });
