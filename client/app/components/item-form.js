c6b5600cdf63cb7175456e2bd52962ffc1f1aa27
Brandan Lennox <brandan@bclennox.com>
2016-09-15 21:41:52 -0400

    Validate item form before submitting.

--- client/app/components/item-form.js
+++ client/app/components/item-form.js
@@ -3,6 +3,10 @@ import Ember from 'ember';
 export default Ember.Component.extend({
   store: Ember.inject.service(),
 
+  isDisabled: Ember.computed('personId', 'itemValue', function (){
+    return !this.get('personId') || !this.get('itemValue');
+  }),
+
   actions: {
     personIdChanged(personId) {
       this.set('personId', personId);
@@ -10,6 +14,7 @@ export default Ember.Component.extend({
 
     add() {
       var self = this;
+
       self.get('store').findRecord('person', self.get('personId')).then(function (person){
         var item = self.get('store').createRecord('item', { trip: self.get('trip'), person: person, value: self.get('itemValue') });
         item.save();
